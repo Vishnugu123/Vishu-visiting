9a8c8a2d9742019d74e5892340792198a0071665
Vishnugu123 <akshaygupta7699181@gmail.com>
2026-03-12 12:57:08 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,10 @@
+window.addEventListener("load", function(){
+
+setTimeout(function(){
+
+document.getElementById("loader").style.display="none";
+document.getElementById("main-content").style.display="block";
+
+}, 2500);
+
+});
